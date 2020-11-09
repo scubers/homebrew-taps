686393d43970ea48fbf6bef671ab08bb92308d1c
jrw0ng <jr-wong@qq.com>
2020-11-09 21:23:45 +0800

    Update and rename i18n-cli_0.0.2.rb to i18n-cli_0.0.4.rb

--- i18n-cli_0.0.4.rb
+++ i18n-cli_0.0.4.rb
@@ -1,11 +1,11 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class I18nCli002 < Formula
+class I18nCli004 < Formula
   desc ""
   homepage ""
-  url "https://raw.githubusercontent.com/scubers/homebrew-i18n-cli/main/i18n-cli_0.0.2.tar.gz"
-  sha256 "b56d60516e38b5a087eaf1b1b0e35b8a8a414deaade8bf9486add60ef266a6f2"
+  url "https://raw.githubusercontent.com/scubers/homebrew-i18n-cli/main/i18n-cli_0.0.4.tar.gz"
+  sha256 "39276f479d3fd7d8287a1c3cc0d58b8083b69aef402c3c2192de18c106a40b27"
   license ""
 
   # depends_on "cmake" => :build
